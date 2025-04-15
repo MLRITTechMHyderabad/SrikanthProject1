b0547abe3d1a3abd4a74d7bf5638fce500c49713
Ande Srikanth <srikanthande76@gmail.com>
2025-04-15 19:35:28 +0530

    amazonBestSeller project done

---
+++ project.sql
@@ -0,0 +1,14 @@
+use amazonbestseller;
+-- product_title	product_price	product_star_rating	product_num_ratings	rank	country
+create table amazonData(
+	product_title varchar(25) Not null,
+    product_price DECIMAL(10,2) Not null,
+    product_star_rating DECIMAL(10.2) Not null,
+    product_num_ratings DECIMAL(10,2) NOT NULL,
+    countryRank INT PRIMARY KEY
+);
+alter table amazondata add column country varchar(30);
+ALTER TABLE amazonData MODIFY product_title VARCHAR(255);
+drop table amazondata;
+describe table amazondata;
+show tables;
