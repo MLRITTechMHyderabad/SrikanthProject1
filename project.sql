9ecebbedca8dc0cec163509203d909715fabea7e
Srikanth Ande <162801452+Srikanth9u@users.noreply.github.com>
2025-04-17 21:08:56 +0530

    sql queries modified

--- project.sql
+++ project.sql
@@ -1,11 +1,12 @@
 use amazonbestseller;
 -- product_title	product_price	product_star_rating	product_num_ratings	rank	country
-create table amazonData(
-	product_title varchar(25) Not null,
+create table amazon(
+	product_title varchar(255) Not null,
     product_price DECIMAL(10,2) Not null,
-    product_star_rating DECIMAL(10.2) Not null,
+    product_star_rating DECIMAL(10,2) Not null,
     product_num_ratings DECIMAL(10,2) NOT NULL,
-    countryRank INT PRIMARY KEY
+    countryRank INT,
+    country varchar(40)
 );
 alter table amazondata add column country varchar(30);
 ALTER TABLE amazonData MODIFY product_title VARCHAR(255);
